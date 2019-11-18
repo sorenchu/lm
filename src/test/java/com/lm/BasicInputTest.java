b5d291279c8cebd2bc4b81d8e5a1530463c396da
sorenchu <antonioostras@gmail.com>
2019-11-18 20:50:51 +0100

    adding test suite declaration

--- src/test/java/com/lm/BasicInputTest.java
+++ src/test/java/com/lm/BasicInputTest.java
@@ -1,18 +1,23 @@
 package com.lm;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class BasicInputTest extends TestCase {
-	public void InputOneTest() {
-		String input1 = "1 book at 12.49\n" + 
-				"1 music CD at 14.99\n" + 
-				"1 chocolate bar at 0.85";
-		ReceiptCalculator receiptCalculator = new ReceiptCalculator(input1);
-		String output = receiptCalculator.execute();
-		assertEquals("1 book : 12.49\n" +
-				"1 music CD: 16.49\n" +
-				"1 chocolate bar: 0.85\n" +
-				"Sales Taxes: 1.50\n" +
-				"Total: 29.83", output);
-	}
+    public void InputOneTest() {
+        String input1 = "1 book at 12.49\n" + "1 music CD at 14.99\n" + "1 chocolate bar at 0.85";
+        ReceiptCalculator receiptCalculator = new ReceiptCalculator(input1);
+        String output = receiptCalculator.execute();
+        assertEquals("1 book : 12.49\n" + "1 music CD: 16.49\n" + "1 chocolate bar: 0.85\n" + "Sales Taxes: 1.50\n"
+                + "Total: 29.83", output);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BasicInputTest.class );
+    }
 }
