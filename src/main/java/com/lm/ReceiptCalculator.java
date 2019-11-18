39d8e9fbd5e62bb8206583b5274a78a30fb9d79d
sorenchu <antonioostras@gmail.com>
2019-11-18 20:51:08 +0100

    changing indentation to whitespaces

--- src/main/java/com/lm/ReceiptCalculator.java
+++ src/main/java/com/lm/ReceiptCalculator.java
@@ -1,14 +1,14 @@
 package com.lm;
 
 public class ReceiptCalculator {
-	private String content;
-	
-	public ReceiptCalculator(String filecontent) {
-		content = filecontent;
-	}
-	
-	public String execute() {
-		String output = "patata";
-		return output;
-	}
+    private String content;
+
+    public ReceiptCalculator(String filecontent) {
+        content = filecontent;
+    }
+
+    public String execute() {
+        String output = "patata";
+        return output;
+    }
 }
