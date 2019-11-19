6e95ccd2c777b93e091a53741849cd00a33e886c
sorenchu <antonioostras@gmail.com>
2019-11-19 08:31:22 +0100

    refactoring Product class to match with ProductTest

--- src/main/java/com/lm/Product.java
+++ src/main/java/com/lm/Product.java
@@ -1,19 +1,31 @@
 package com.lm;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Product {
     private int quantity;
     private String name;
     private boolean imported;
     private double price;
-    
-    public Product(String info) {
-        // \d+\s+([a-zA-Z]+(\s)+)*at\s\d+(\.)?\d+
+
+    public Product() {
         this.quantity = 0;
-        this.name = "patata";
+        this.name = "empty";
         this.imported = false;
         this.price = 0.0;
     }
-    
+
+    public Product(String info) {
+        // FIXME: it may be useful to check first
+        // if regex is correct
+        // \d+\s+([a-zA-Z]+(\s)+)*at\s\d+(\.)?\d+
+        this.setQuantityThroughInfo(info);
+        this.setNameThroughInfo(info);
+        this.setImported();
+        this.setPrice(info);
+    }
+
     public int getQuantity() {
         return this.quantity;
     }
@@ -25,8 +37,37 @@ public class Product {
     public boolean isImported() {
         return this.imported;
     }
-    
+
     public double getPrice() {
         return this.price;
     }
+
+    private void setQuantityThroughInfo(String info) {
+        this.quantity = Integer.valueOf(this.parseRegex(info, "^\\d+"));
+    }
+
+    private void setNameThroughInfo(String info) {
+        this.name = this.parseRegex(info, "([a-zA-Z]+(\\s)+)*at");
+        this.name = this.name.replaceAll(" at", "");
+    }
+
+    private void setImported() {
+        // FIXME: think if "imported" can have a capitalized letter
+        this.imported = name.contains("imported");
+    }
+
+    private void setPrice(String info) {
+        this.price = Double.valueOf(this.parseRegex(info, "\\d+\\.\\d+"));
+    }
+
+    private String parseRegex(String info, String pattern) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(info);
+        String value = "";
+        while (matcher.find()) {
+            value = matcher.group();
+        }
+        return value;
+    }
+
 }
--- src/test/java/com/lm/ProductTest.java
+++ src/test/java/com/lm/ProductTest.java
@@ -5,17 +5,17 @@ import junit.framework.TestCase;
 public class ProductTest extends TestCase {
     public void testCreationFromOneWordProductLine() {
         Product product = new Product("1 book at 12.49");
-        assertEquals(product.getQuantity(), 1);
-        assertEquals(product.getName(), "book");
-        assertEquals(product.getPrice(), 12.49);
+        assertEquals(1, product.getQuantity());
+        assertEquals("book", product.getName());
+        assertEquals(12.49, product.getPrice());
         assertFalse(product.isImported());
     }
-    
+
     public void testCreationFromTwoWordsProductLine() {
         Product product = new Product("1 music CD at 14.99");
-        assertEquals(product.getQuantity(), 1);
-        assertEquals(product.getName(), "music CD");
-        assertEquals(product.getPrice(), 14.99);
+        assertEquals(1, product.getQuantity());
+        assertEquals("music CD", product.getName());
+        assertEquals(14.99, product.getPrice());
         assertFalse(product.isImported());
     }
 }
