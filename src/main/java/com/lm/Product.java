5d6b6711dc0f2bd08fb2c21413d2007e9606511d
sorenchu <antonioostras@gmail.com>
2019-11-21 19:36:50 +0100

    tweaking product to cope with new price classes

--- src/main/java/com/lm/Product.java
+++ src/main/java/com/lm/Product.java
@@ -3,91 +3,45 @@ package com.lm;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.lm.price.Price;
+import com.lm.price.PriceFactory;
+
 public class Product {
-    private static double IMPORTED_TAX = 0.05;
-    private static double REGULAR_TAX = 0.1;
-
-    private int quantity;
     private String name;
-    private boolean imported;
-    private boolean exempted;
-
-    // FIXME: this is a different object
-    private double price;
-    private double taxes;
-
-    // FIXME: this should be in a configuration file
-    private static String exemptedList[] = { "chocolate", "headache pills", "book" };
+    private Price price;
 
     public Product() {
-        this.quantity = 0;
         this.name = "empty";
-        this.imported = false;
-        this.exempted = false;
-        this.price = 0.0;
-        this.taxes = 0.0;
+        this.price = new Price();
     }
 
     public Product(String info) {
         // FIXME: it may be useful to check first
         // if regex is correct
         // \d+\s+([a-zA-Z]+(\s)+)*at\s\d+(\.)?\d+
+        this.setPrice(info);
         this.setQuantityThroughInfo(info);
         this.setNameThroughInfo(info);
-        this.setImported();
-        this.setExepmted();
-        this.setPrice(info);
     }
 
     static public String formatNumber(double number) {
         return (String.format("%.02f", number)).replace(",", ".");
     }
 
-    public int getQuantity() {
-        return this.quantity;
-    }
-
     public String getName() {
         return this.name;
     }
-
-    public boolean isImported() {
-        return this.imported;
+    
+    public String print() {
+        return this.price.getQuantity() + " " + this.getName() + ": " + Product.formatNumber(this.price.getTotalPrice()) + "\n";
     }
-
-    public boolean isExempted() {
-        return this.exempted;
-    }
-
-    public void calculateTaxes() {
-        this.taxes = 0;
-        if (this.isImported()) {
-            this.taxes += this.getRoundedTaxes(IMPORTED_TAX);
-        }
-        if (!this.isExempted()) {
-            this.taxes += this.getRoundedTaxes(REGULAR_TAX);
-        }
-    }
-
-    public double getPrice() {
+    
+    public Price getPrice() {
         return this.price;
     }
 
-    public double getTotalPrice() {
-        this.calculateTaxes();
-        return (this.price + this.taxes);
-    }
-
-    public double getTaxes() {
-        return this.taxes;
-    }
-
-    public String print() {
-        return this.getQuantity() + " " + this.getName() + ": " + this.formatNumber(this.getTotalPrice()) + "\n";
-    }
-
     private void setQuantityThroughInfo(String info) {
-        this.quantity = Integer.valueOf(this.parseRegex(info, "^\\d+"));
+        this.price.setQuantity(Integer.valueOf(this.parseRegex(info, "^\\d+")));
     }
 
     private void setNameThroughInfo(String info) {
@@ -95,22 +49,10 @@ public class Product {
         this.name = this.name.replaceAll(" at", "");
     }
 
-    private void setImported() {
-        // FIXME: think if "imported" can have a capitalized letter
-        this.imported = name.contains("imported");
-    }
-
     private void setPrice(String info) {
-        this.price = Double.valueOf(this.parseRegex(info, "\\d+\\.\\d+"));
-    }
-
-    private void setExepmted() {
-        for (String keyword : this.exemptedList) {
-            if (name.contains(keyword)) {
-                this.exempted = true;
-                return;
-            }
-        }
+        PriceFactory priceFactory = new PriceFactory();
+        this.price = priceFactory.getPrice(info);
+        this.price.setPrice(Double.valueOf(this.parseRegex(info, "\\d+\\.\\d+")));
     }
 
     private String parseRegex(String info, String pattern) {
@@ -121,9 +63,5 @@ public class Product {
             value = matcher.group();
         }
         return value;
-    }
-
-    private double getRoundedTaxes(double taxPercentage) {
-        return (double) Math.round(this.price * taxPercentage * 100d) / 100d;
     }
 }
--- src/test/java/com/lm/ProductTest.java
+++ src/test/java/com/lm/ProductTest.java
@@ -11,28 +11,32 @@ public class ProductTest extends TestCase {
 
     public void testCreationFromOneWordProductLine() {
         Product product = new Product("1 book at 12.49");
-        assertEquals(1, product.getQuantity());
+        assertEquals(1, product.getPrice().getQuantity());
         assertEquals("book", product.getName());
-        assertEquals(12.49, product.getPrice());
-        assertFalse(product.isImported());
+        assertEquals(12.49, product.getPrice().getTotalPrice());
     }
 
     public void testCreationFromTwoWordsProductLine() {
         Product product = new Product("1 music CD at 14.99");
-        assertEquals(1, product.getQuantity());
+        assertEquals(1, product.getPrice().getQuantity());
         assertEquals("music CD", product.getName());
-        assertEquals(14.99, product.getPrice());
-        assertFalse(product.isImported());
+        assertEquals(16.49, product.getPrice().getTotalPrice());
     }
-    
+
+    public void testCreationFromTwoWordsProductsLine() {
+        Product product = new Product("3 music CDs at 14.99");
+        assertEquals(3, product.getPrice().getQuantity());
+        assertEquals("music CDs", product.getName());
+        assertEquals(49.47, product.getPrice().getTotalPrice());
+    }
+
     public void testCreationSeveralWordsImported() {
         Product product = new Product("1 imported box of chocolates at 10.00");
-        assertEquals(1, product.getQuantity());
+        assertEquals(1, product.getPrice().getQuantity());
         assertEquals("imported box of chocolates", product.getName());
-        assertEquals(10.00, product.getPrice());
-        assertTrue(product.isImported());
+        assertEquals(10.5, product.getPrice().getTotalPrice());
     }
-    
+
     public static Test suite() {
         return new TestSuite(ProductTest.class);
     }
